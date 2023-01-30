92850577b5b643c8b52c235975a47230358e396c
Niall <niallos11@gmail.com>
2023-01-30 21:59:15 +0000

    Fixed missing products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -22,12 +24,14 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -38,8 +42,8 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")  # noqa
                 return redirect(reverse('products'))
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)  # noqa
-            products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
