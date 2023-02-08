044cbca0451d26742d83fe58a352fede65db6a04
Niall <niallos11@gmail.com>
2023-02-08 14:39:31 +0000

    Stripe Part 2

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
+        'stripe_public_key': 'pk_test_51MZ0qIDv9XsEGmY4ZuaQwslBxwbt6m59NgO9nQQY974l4Vwvy62UeFX3FpRvLvOrSrJ9uVU5Pdmy4QYoICkkGx0L00myovoVCK',  # noqa
         'client_secret': 'test client secret',
     }
 
